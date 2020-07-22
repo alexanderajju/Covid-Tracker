76d1df4d5d5069bdc1675aa2f0e9379154184946
alexanderajju <alexanderajju@gmail.com>
2020-07-22 09:37:24 +0530

    Pop-up

--- src/util.js
+++ src/util.js
@@ -43,6 +43,18 @@ export const showDataOnMap = (data, casesType = "cases") =>
         Math.sqrt(country[casesType]) * casesTypeColors[casesType].multiplier
       }
     >
-      <Popup>Im a Popup</Popup>
+      <Popup>
+        <div className="info-container" >
+          <div className="info-flag"
+          style={{backgroundImage:`url(${country.countryInfo.flag})`}}
+          />
+          <div
+          className="info-name"
+          >{country.country}</div>
+    <div className="info-confirmed" >Cases : {numeral(country.cases).format("0,0")}</div>
+          <div className="info-recovered" >Recovered : {numeral(country.recovered).format("0,0")}</div>
+          <div className="info-deaths" >Death : {numeral(country.deaths).format("0,0")}</div>
+        </div>
+      </Popup>
     </Circle>
   ));
